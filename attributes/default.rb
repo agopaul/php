77cbb1f2b790f26c5c77ac392f0c7270b9298426
Paolo Agostinetto <paul.ago@gmail.com>
2013-12-03 18:14:17 +0100

    Ported php.ini tpl with attributes from priestjim/chef-php

--- attributes/default.rb
+++ attributes/default.rb
@@ -55,6 +55,29 @@ else
   default['php']['packages']      = ['php5-cgi', 'php5', 'php5-dev', 'php5-cli', 'php-pear']
 end
 
+# PHP.ini Settings
+default['php']['ini_settings'] = {
+  'short_open_tag' => 'On',
+  'open_basedir' => '',
+  'max_execution_time' => '300',
+  'max_input_time' => '300',
+  'memory_limit' => '128M',
+  'error_reporting' => 'E_ALL & ~E_DEPRECATED & ~E_NOTICE',
+  'display_errors' => 'Off',
+  'error_log' => '',
+  'register_globals' => 'Off',
+  'register_long_arrays' => 'Off',
+  'post_max_size' => '32M',
+  'magic_quotes_gpc' => 'Off',
+  'allow_url_fopen' => 'On',
+  'allow_url_include' => 'Off',
+  'always_populate_raw_post_data' => 'Off',
+  'cgi.fix_pathinfo' => '1',
+  'upload_max_filesize' => '32M',
+  'date.timezone' => 'Europe/Athens',
+  'session.cookie_httponly' => '0'
+}
+
 default['php']['url'] = 'http://us.php.net/distributions'
 default['php']['version'] = '5.3.10'
 default['php']['checksum'] = 'ee26ff003eaeaefb649735980d9ef1ffad3ea8c2836e6ad520de598da225eaab'
